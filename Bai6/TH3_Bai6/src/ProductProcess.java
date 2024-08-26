9da202480c3412aaac5cc47d9363376f1ea75f7a
Trangne2901 <tranthuyentrang29012005@gmail.com>
2024-08-26 11:17:44 +0700

    lab6

---
+++ Bai6/TH3_Bai6/src/ProductProcess.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class ProductProcess {
+
+    public static boolean writeObjectData(Product[] products, String fileName) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            for (Product product : products) {
+                oos.writeObject(product);
+            }
+            return true; // Trả về true nếu ghi dữ liệu thành công
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false; // Trả về false nếu có lỗi xảy ra
+        }
+    }
+
+    public Product[] readObjectData(String fileName) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+            // Đọc các đối tượng từ file vào danh sách
+            Product[] products = new Product[2];
+            for (int i = 0; i < products.length; i++) {
+                products[i] = (Product) ois.readObject();
+            }
+            return products; // Trả về mảng đối tượng
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null; // Trả về null nếu có lỗi xảy ra hoặc file không có dữ liệu
+        }
+    }
+}
